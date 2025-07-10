242df01658ff52d0aec84bfb66e48e7dfb8e2250
guimmotta <aguinaldoammotta@gmail.com>
2025-07-10 15:39:15 -0300

    removed register importation in reducers/tasks

--- src/store/reducers/tasks.ts
+++ src/store/reducers/tasks.ts
@@ -1,7 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import Task from '../../models/Task'
 import * as enums from '../../utils/enums/Task'
-import Register from '../../pages/register'
 
 type TasksState = {
   itens: Task[]
